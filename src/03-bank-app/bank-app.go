0ff688a459d7abd41689eed0df87f96888f77cc2
Serj Komarovsky <serj.komarovsky@gmail.com>
2025-02-18 12:58:18 +0300

    infinite loop

--- src/03-bank-app/bank-app.go
+++ src/03-bank-app/bank-app.go
@@ -2,55 +2,34 @@ package main
 
 import "fmt"
 
+
 func main() {
 	var choice int
 	var accountBalance float64 = 1000
 
-	chooseOperation(&choice)
-	fmt.Printf("Your choice: %v\n", choice)
+    fmt.Println("Welcome to Go Bank!")
+    for choice != 4 {
+        chooseOperation(&choice)
 
-	wantsCheckBalance := choice == 1
-	wantsDepositMoney := choice == 2
-	wantsWithdrawMoney := choice == 3
-
-	if wantsCheckBalance {
-		fmt.Printf("Your balance is %.2f$\n", accountBalance)
-	} else if wantsDepositMoney {
-		var depositValue float64
-		fmt.Print("Enter the amount you want to deposit, $: ")
-		fmt.Scan(&depositValue)
-
-        if depositValue <= 0 {
-            fmt.Print("Deposit value must be greater than 0")
-            return
+        switch choice {
+            case 1:
+                fmt.Printf("Your balance is %.2f$\n", accountBalance)
+            case 2:
+                depositMoney(&accountBalance)
+            case 3:
+                withdrawMoney(&accountBalance)
+            case 4:
+                return
+            default:
+                fmt.Println("invalid operation")
         }
-		accountBalance += depositValue
-		fmt.Printf("Balance updated! New amount: %.2f$\n", accountBalance)
-	} else if wantsWithdrawMoney {
-		var withdrawValue float64
-		fmt.Print("Enter the amount you want to withdraw: ")
-		fmt.Scan(&withdrawValue)
+    }
 
-        if withdrawValue <= 0 {
-            fmt.Print("Withdrawal value must be greater than 0")
-            return
-        }
-
-        if withdrawValue > accountBalance {
-            fmt.Print("Withdrawal value must be less than current balance")
-            return
-        }
-
-		accountBalance -= withdrawValue
-		fmt.Printf("You've successfully withdrawed %.2f$! Current balance: %.2f$\n", withdrawValue, accountBalance)
-	} else {
-		fmt.Println("Goodbye!")
-	}
+    fmt.Println(("Thanks for choosing our bank!"))
 }
 
 func chooseOperation(choice *int) {
 	fmt.Print(`
-    Welcome to Go Bank!
     What do you want to do?
     1. Check balance
     2. Deposit money
@@ -59,4 +38,36 @@ func chooseOperation(choice *int) {
     `)
 
 	fmt.Scan(choice)
+}
+
+func withdrawMoney(accountBalance *float64) {
+    var withdrawValue float64
+    fmt.Print("Enter the amount you want to withdraw: ")
+    fmt.Scan(&withdrawValue)
+
+    if withdrawValue <= 0 {
+        fmt.Print("Withdrawal value must be greater than 0")
+        return
+    }
+
+    if withdrawValue > *accountBalance {
+        fmt.Print("Withdrawal value must be less than current balance")
+        return
+    }
+
+    *accountBalance -= withdrawValue
+    fmt.Printf("You've successfully withdrawed %.2f$! Current balance: %.2f$\n", withdrawValue, *accountBalance)
+}
+
+func depositMoney(accountBalance *float64) {
+    var depositValue float64
+    fmt.Print("Enter the amount you want to deposit, $: ")
+    fmt.Scan(&depositValue)
+
+    if depositValue <= 0 {
+        fmt.Print("Deposit value must be greater than 0")
+        return
+    }
+    *accountBalance += depositValue
+    fmt.Printf("Balance updated! New amount: %.2f$\n", *accountBalance)
 }
